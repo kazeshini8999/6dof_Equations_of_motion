7a3426fb4055a237f93d2d2fda2f3fc780ddda00
kazeshini8999 <49817421+kazeshini8999@users.noreply.github.com>
2022-11-23 13:30:36 -0500

    update

--- C2cO.m
+++ C2cO.m
@@ -1,5 +1,5 @@
 function [v2] = C2cO(v1,x) % you need to include t when you input x
 %function to convert vectors from O frame basis to C frame basis
 C2cO = Euler2DCM(x(10:13));
-v2 = OcC2*v1;
+v2 = C2cO*v1;
 end
